7b16c696b67b7df0ebf37d3618728578d37690b8
Carlos dos Santos <cds@cds-software.com.br>
2019-10-17 21:07:22 -0300

    Add ConsoleAp .NET 4.5.2

---
+++ ConsoleAppFul45/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibraryCoreStandard;
+
+namespace ConsoleAppFul45
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is a Console .NET 4.5.2");
+            Console.WriteLine("****************************\n\n");
+            var service = new CustomerService();
+            service.Add(new Customer() { ID = 1, Name = "Carlos" });
+            service.Add(new Customer() { ID = 2, Name = "John" });
+            service.Add(new Customer() { ID = 3, Name = "Mary" });
+
+            Console.WriteLine("Calling Library from .NET Standard Project with .NET 4.5.2");
+            Console.WriteLine("----------------------------------------------------------");
+            foreach (var s in service.GetAll())
+            {
+                Console.WriteLine($"{s.ID} - {s.Name}");
+            }
+        }
+    }
+}
